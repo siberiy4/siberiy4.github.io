ac6804be4a14d73f6f210d4a2ef8ae3b5eb8f8ff
yo-kanyukari <yokari@outlook.jp>
2021-09-21 18:19:56 +0900

    CSSをBLUMAで当てるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,101 @@
 import React from 'react';
 import icon from './img/icon.png';
 import './App.css';
-import "bulma/css/bulma.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter, faAmazon, faGithubAlt, faDiscord } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
 
-    <div className=" ">
-      <div className=" App-header columns is-vcentered">
-        <div className="p-6 column has-text-centered">
-          <figure className="image  is-inline-block">
-            <img src={icon} className="App-logo" alt="icon" />
-          </figure>
-          <p>ID: Siberiy4 </p>
-        </div>
+    <div className=" hero is-fullheight is-default is-bold">
 
-        <div className="column ">
-          <div>
-            <a href="https://twitter.com/siberiy4" target="_blank" rel="noopener noreferrer">
-              <span className="icon-text">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faTwitter} />
-                </span>
-                <span>Twitter</span>
-              </span>
-            </a>
+      <div className="hero-body has-background-success-light">
+        <div className="App-text container has-text-centered">
+          <div className="columns is-vcentered">
+            <div className="column is-7 ">
+              <figure className="image is-5by4">
+                <img src={icon} className="App-logo" alt="icon" />
+              </figure>
+              <div className="title is-2 ">ID: Siberiy4 </div>
+            </div>
+            <div className="buttons are-medium column is-4 is-offset-1 ">
+
+              <div >
+                <button className="my-5 button is-info is-fullwidth">
+                  <a className="has-text-warning-light" href="https://twitter.com/siberiy4" target="_blank" rel="noopener noreferrer">
+                    <span className="icon-text has-text-centered">
+                      <span className="icon">
+                        <FontAwesomeIcon icon={faTwitter} />
+                      </span>
+                      <span>Twitter</span>
+                    </span>
+                  </a>
+                </button>
+              </div>
+
+              <div>
+                <button className="my-5 button is-dark is-fullwidth">
+                  <a className="has-text-warning-light" href="https://github.com/yo-kanyukari" target="_blank" rel="noopener noreferrer">
+                    <span className="icon-text">
+                      <span className="icon">
+                        <FontAwesomeIcon icon={faGithubAlt} />
+                      </span>
+                      <span>GitHub</span>
+                    </span>
+                  </a>
+                </button>
+              </div>
+
+              <div>
+                <button className="my-5 button is-warning is-fullwidth">
+                  <a className="has-text-black-ter" href="https://www.amazon.jp/hz/wishlist/ls/33THA53ST32R1?ref_=wl_share" target="_blank" rel="noopener noreferrer" >
+                    <span className="icon-text">
+                      <span className="icon">
+                        <FontAwesomeIcon icon={faAmazon} />
+                      </span>
+                      <span>Amazon</span>
+                    </span>
+                  </a>
+                </button>
+              </div>
+
+              <div>
+                <button className="my-5 button is-link is-fullwidth">
+                  <span className="icon-text">
+                    <span className="icon">
+                      <FontAwesomeIcon icon={faDiscord} />
+                    </span>
+                    <span>Discord:  siberiy4#1380</span>
+                  </span>
+                </button>
+              </div>
+
+              <div>
+                <button className="my-5 button is-link is-fullwidth">
+                  <a className="has-text-warning-light" href="https://discord.gg/FJcnXtCHfm" target="_blank" rel="noopener noreferrer">
+                    <span className="icon-text">
+                      <span className="icon">
+                        <FontAwesomeIcon icon={faDiscord} />
+                      </span>
+                      <span>Discord Server</span>
+                    </span>
+                  </a>
+                </button>
+              </div>
+
+            </div>
           </div>
-
-          <div>
-            <a href="https://github.com/yo-kanyukari" target="_blank" rel="noopener noreferrer">
-              <span className="icon-text">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faGithubAlt} />
-                </span>
-                <span>GitHub</span>
-              </span>
-            </a>
-          </div>
-
-          <div>
-            <a href="https://www.amazon.jp/hz/wishlist/ls/33THA53ST32R1?ref_=wl_share" target="_blank" rel="noopener noreferrer">
-              <span className="icon-text">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faAmazon} />
-                </span>
-                <span>Amazon</span>
-              </span>
-            </a>
-          </div>
-
-          <div>
-            <span className="icon-text">
-              <span className="icon">
-                <FontAwesomeIcon icon={faDiscord} />
-              </span>
-              <span>Discord ID: siberiy4#1380</span>
-            </span>
-          </div>
-
-          <div>
-            <a href="https://discord.gg/FJcnXtCHfm" target="_blank" rel="noopener noreferrer">
-              <span className="icon-text">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faDiscord} />
-                </span>
-                <span>Discord Server</span>
-              </span>
-            </a>
-          </div>
-
         </div>
       </div>
     </div>
-
-
   );
 }
 
-function copyToClip(text: string) {
-  navigator.clipboard.writeText(text);
-  alert(text + 'をクリップボードにコピーしました')
-}
+
+
+// function copyToClip(text: string) {
+//   navigator.clipboard.writeText(text);
+//   alert(text + 'をクリップボードにコピーしました')
+// }
 
 export default App;
